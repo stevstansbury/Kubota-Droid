0116a4befafb1be61a91e188873e3682219b3b19
Rob Caraway <rcaraway@inmotionsoftware.com>
2018-12-19 11:10:33 -0600

    added interface for calling single guide pages

--- network/src/main/java/com/kubota/network/service/GuideService.kt
+++ network/src/main/java/com/kubota/network/service/GuideService.kt
@@ -1,14 +1,15 @@
 package com.kubota.network.service
 
+import com.kubota.network.model.GuidePage
 import com.microsoft.azure.android
 import com.microsoft.azure.storage.ResultSegment
 import com.microsoft.azure.storage.blob.BlobInputStream
+import com.microsoft.azure.storage.blob.CloudBlobDirectory
 import retrofit2.Call
 
 
 interface GuideService {
-
     fun getGuideList(model: String): Call<ResultSegment<String>>
-    
-
+    fun getGuidePages(model: String, guideName: String): Call<Array<GuidePage>>
+    fun getGuidePages(guidePrefix: String): Call<Array<GuidePage>>
 }
