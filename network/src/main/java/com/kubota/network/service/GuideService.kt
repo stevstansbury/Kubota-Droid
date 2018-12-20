8863a93f05e7fd7193633446019255647cf2b413
Rob Caraway <rcaraway@inmotionsoftware.com>
2018-12-20 11:40:41 -0600

    Guides blob ready to be tested

--- network/src/main/java/com/kubota/network/service/GuideService.kt
+++ network/src/main/java/com/kubota/network/service/GuideService.kt
@@ -1,15 +1,10 @@
 package com.kubota.network.service
 
 import com.kubota.network.model.GuidePage
-import com.microsoft.azure.android
-import com.microsoft.azure.storage.ResultSegment
-import com.microsoft.azure.storage.blob.BlobInputStream
-import com.microsoft.azure.storage.blob.CloudBlobDirectory
+import com.microsoft.azure.storage.blob.ListBlobItem
 import retrofit2.Call
 
-
 interface GuideService {
-    fun getGuideList(model: String): Call<ResultSegment<String>>
-    fun getGuidePages(model: String, guideName: String): Call<Array<GuidePage>>
-    fun getGuidePages(guidePrefix: String): Call<Array<GuidePage>>
+    fun getGuideList(model: String): Call<List<ListBlobItem>?>
+    fun getGuidePages(guidePrefix: String): Call<List<GuidePage>>
 }
