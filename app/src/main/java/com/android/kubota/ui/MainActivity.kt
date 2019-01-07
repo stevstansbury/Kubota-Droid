34be6cef42ff35678bc243f28f851b6fa3819184
Rob Caraway <robbor911@gmail.com>
2019-01-07 12:13:54 -0600

    Created a base screen to plug screens into (#11)
    * setup temp home screen to plug other screens into
    
    * deleted log import
    
    * cleaned up spacing
    
    * cleaned up signup and other stuff

--- app/src/main/java/com/android/kubota/ui/MainActivity.kt
+++ app/src/main/java/com/android/kubota/ui/MainActivity.kt
@@ -1,35 +1,34 @@
 package com.android.kubota.ui
 
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import com.android.kubota.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupButtons()
+    }
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    private fun setupButtons() {
+        homeButton.setOnClickListener {
+            //Launch home screen here
+        }
+        manualsButton.setOnClickListener{
+            //Launch manual screen here
+        }
+        guidesButton.setOnClickListener{
+            //Launch guide screen here
+        }
+        dealerLocatorButton.setOnClickListener{
+            //Launch Dealer Locator screen here
+        }
+        profileButton.setOnClickListener{
+            //Launch Profile screen here
+        }
     }
 }
