95dbba5359e82f78414c2ec52566a8829e039476
James Hunt <39564414+jhunt-inmotion@users.noreply.github.com>
2021-01-18 12:10:22 -0600

    TM-442: Deeplinks (#300)
    * alert notification are able to take you to telematics
    
    * Fault code and warranty deeplinks with notification screen first page you land on
    
    * open maintenance schedule deeplink
    
    * cleanup
    
    * Deeplink to geofence
    
    * retrieve inbox message rather than create a matching one for deeplink
    
    * cleanup
    
    * cleanup

--- app/src/main/java/com/android/kubota/ui/MainActivity.kt
+++ app/src/main/java/com/android/kubota/ui/MainActivity.kt
@@ -16,16 +16,20 @@ import com.android.kubota.R
 import com.android.kubota.app.AppProxy
 import com.android.kubota.extensions.hideKeyboard
 import com.android.kubota.ui.equipment.EquipmentDetailFragment
+import com.android.kubota.ui.notification.NotificationDetailFragment
 import com.android.kubota.ui.resources.EquipmentModelDetailFragment
 import com.android.kubota.viewmodel.equipment.EquipmentListViewModel
 import com.android.kubota.viewmodel.resources.EquipmentCategoriesViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.snackbar.Snackbar
+import com.inmotionsoftware.promisekt.done
 import com.inmotionsoftware.promisekt.ensure
+import com.inmotionsoftware.promisekt.map
 import com.kubota.service.domain.EquipmentModel
 import com.kubota.service.domain.EquipmentUnit
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.kubota_toolbar_with_logo.*
+import java.util.*
 
 private const val LOG_IN_REQUEST_CODE = 1
 private const val SELECTED_TAB = "selected_tab"
@@ -56,6 +60,11 @@ class MainActivity : BaseActivity(), TabbedControlledActivity, TabbedActivity, A
 
     private val equipmentCategoriesViewModel: EquipmentCategoriesViewModel by lazy {
         EquipmentCategoriesViewModel.instance(owner = this)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        intent?.let { handleDeepLink(it) }
     }
 
     @SuppressLint("NewApi")
@@ -113,6 +122,8 @@ class MainActivity : BaseActivity(), TabbedControlledActivity, TabbedActivity, A
 
         this.equipmentListViewModel.updateData(this)
         this.equipmentCategoriesViewModel.updateData()
+
+        intent?.let { handleDeepLink(it) }
     }
 
     override fun onResume() {
@@ -161,6 +172,17 @@ class MainActivity : BaseActivity(), TabbedControlledActivity, TabbedActivity, A
         if (!didGoBack) {
             super.onBackPressed()
         }
+    }
+
+    private fun handleDeepLink(intent: Intent) {
+        val messageId = intent.extras?.getString("messageId")?.let { UUID.fromString(it) }
+
+        AppProxy.proxy.accountManager.isAuthenticated.observe(this, {
+            AppProxy.proxy.serviceManager.userPreferenceService
+                .getInbox(null, null)
+                .map { inboxMessages -> inboxMessages.first { it.id == messageId } }
+                .done { addFragmentToBackStack(NotificationDetailFragment.createInstance(it)) }
+        })
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
--- app/src/main/java/com/android/kubota/ui/notification/NotificationDetailFragment.kt
+++ app/src/main/java/com/android/kubota/ui/notification/NotificationDetailFragment.kt
@@ -9,11 +9,23 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.android.kubota.R
+import com.android.kubota.app.AppProxy
 import com.android.kubota.databinding.FragmentNotificationDetailBinding
+import com.android.kubota.ui.FlowActivity
+import com.android.kubota.ui.MaintenanceIntervalFragment
+import com.android.kubota.ui.equipment.EquipmentDetailFragment
+import com.android.kubota.ui.equipment.FaultCodeInquiryFragment
+import com.android.kubota.ui.equipment.FaultCodeResultsFragment
+import com.android.kubota.ui.equipment.TelematicsFragment
+import com.android.kubota.ui.geofence.GeofenceFragment
 import com.android.kubota.utility.AuthDelegate
 import com.android.kubota.viewmodel.notification.NotificationsViewModel
+import com.inmotionsoftware.promisekt.Promise
+import com.inmotionsoftware.promisekt.map
 import com.kubota.service.domain.InboxMessage
 import com.kubota.service.domain.InboxMessageSource
+import kotlinx.android.synthetic.main.fragment_notification_detail.*
+import java.util.*
 
 private val KEY_NOTIFICATION = "notification"
 
@@ -22,8 +34,7 @@ class NotificationDetailFragment: Fragment() {
     private var b: FragmentNotificationDetailBinding? = null
     private val binding get() = b!!
     private val notification: InboxMessage by lazy {
-        val temp: InboxMessage = requireArguments().getParcelable(KEY_NOTIFICATION)!!
-        temp
+        requireArguments().getParcelable(KEY_NOTIFICATION)!!
     }
     private val viewModel: NotificationsViewModel by activityViewModels()
 
@@ -79,11 +90,62 @@ class NotificationDetailFragment: Fragment() {
         if (savedInstanceState == null && !notification.isRead) {
             viewModel.markAsRead(authDelegate, notification)
         }
+
+        if (notification.deepLink.isEmpty()) {
+            deepLinkText.visibility = View.GONE
+        } else {
+            deepLinkText.text = getLinkText(notification.deepLink["alertId"])
+        }
+
+        deepLinkText.setOnClickListener {
+            showDeepLinkData(notification.deepLink)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         b = null
+    }
+
+    private fun showDeepLinkData(deepLink: Map<String, String>) {
+        val equipmentId = UUID.fromString(deepLink["exposedEquipmentId"])
+        val getEquipmentUnit = {
+            AppProxy.proxy.serviceManager.userPreferenceService
+                .getEquipmentUnit(equipmentId)
+                .map { it!! }
+        }
+        when (deepLink["alertId"]) {
+            "BAT-L-LOW",
+            "COOL-T-HIGH",
+            "DEF-L-LOW",
+            "HYDR-T-HIGH",
+            "FUEL-L-LOW" -> Promise.value(TelematicsFragment.createInstance(equipmentId))
+            "FAULT-CODE" -> getEquipmentUnit().map {
+                    when(it.telematics?.faultCodes?.size) {
+                        1 -> FaultCodeResultsFragment.createInstance(it.telematics!!.faultCodes.first())
+                        else -> FaultCodeInquiryFragment.createInstance(it)
+                    }
+                }
+            "MAINTENANCE" -> getEquipmentUnit().map { MaintenanceIntervalFragment.createInstance(it.model) }
+            "TRANSPORT" -> getEquipmentUnit().map { GeofenceFragment.createInstance(it.telematics?.location) }
+            "WARRANTY" -> getEquipmentUnit().map { EquipmentDetailFragment.createInstance(it) }
+            else -> activity?.onBackPressed().let { null }
+        }?.map { fragment -> (activity as? FlowActivity)?.addFragmentToBackStack(fragment) }
+    }
+
+    private fun getLinkText(alertId: String?): String? {
+        return context?.getString(when (alertId) {
+            "BAT-L-LOW" -> R.string.view_battery_level
+            "COOL-T-HIGH" -> R.string.view_coolant_temp
+            "DEF-L-LOW" -> R.string.view_def_level
+            "HYDR-T-HIGH" -> R.string.view_hydraulic_temp
+            "FUEL-L-LOW" -> R.string.view_fuel_level
+            "FAULT-CODE" -> R.string.view_fault_code
+            "MAINTENANCE" -> R.string.view_maintenance_schedule
+            "TRANSPORT" -> R.string.view_location
+            "WARRANTY" -> R.string.view_warranty_info
+            else -> return null
+        })
     }
 
     companion object {
