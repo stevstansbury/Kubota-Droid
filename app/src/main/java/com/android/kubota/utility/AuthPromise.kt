991d05134bbb8273f6ce7567616a5904f296b36a
James Hunt <39564414+jhunt-inmotion@users.noreply.github.com>
2021-09-23 09:28:59 -0500

    TM-875: Fixed reAuth bug that was happening due to a race condition. Now checking if the token is expired before sending the request, and if token is expired, will get a new token one time (#388)

--- app/src/main/java/com/android/kubota/utility/AuthPromise.kt
+++ app/src/main/java/com/android/kubota/utility/AuthPromise.kt
@@ -1,9 +1,14 @@
 package com.android.kubota.utility
 
+import android.util.Base64
 import com.android.kubota.app.AppProxy
+import com.inmotionsoftware.foundation.concurrent.DispatchExecutor
 import com.inmotionsoftware.promisekt.*
 import com.kubota.service.api.KubotaServiceError
+import com.squareup.moshi.Moshi
 import java.lang.ref.WeakReference
+import java.util.*
+import java.util.concurrent.Semaphore
 
 interface AuthDelegate {
     fun authenticateOnSessionExpired(): Promise<Boolean>
@@ -11,11 +16,17 @@ interface AuthDelegate {
 
 class AuthPromise(delegate: AuthDelegate? = null) {
 
+    private companion object {
+        val semaphore = Semaphore(1)
+        val moshi: Moshi = Moshi.Builder().build()
+    }
+
     private val delegate: WeakReference<AuthDelegate>? =
-                            if (delegate != null) WeakReference(delegate) else null
+        if (delegate != null) WeakReference(delegate) else null
 
     fun <T> then(execute: () -> Promise<T>): Promise<T> {
-        return execute()
+        return reAuthIfAboutToExpire()
+            .thenMap { execute() }
             .recover { error ->
                 when (error) {
                     is KubotaServiceError.Unauthorized ->
@@ -40,4 +51,38 @@ class AuthPromise(delegate: AuthDelegate? = null) {
             }
     }
 
+    private data class TokenExpiration(val exp: Long)
+
+    private fun reAuthIfAboutToExpire(): Promise<Unit> {
+        return noConcurrentReAuth {
+            val accessToken = AppProxy.proxy.accountManager.authToken?.accessToken
+                ?: return@noConcurrentReAuth Promise.value(Unit)
+
+            val dataPart = accessToken
+                .substringAfter(".")
+                .substringBeforeLast(".")
+
+            val decoded = Base64.decode(dataPart, Base64.DEFAULT).toString(Charsets.UTF_8)
+
+            val expirationUnixSeconds =
+                moshi.adapter(TokenExpiration::class.java).fromJson(decoded)!!.exp
+
+            if (expirationUnixSeconds < (System.currentTimeMillis() / 1000) - 30) {
+                AppProxy.proxy.accountManager.reauthenticate()
+            } else {
+                Promise.value(Unit)
+            }
+        }
+    }
+
+    private fun <T> noConcurrentReAuth(execute: () -> Promise<T>): Promise<T> {
+        return Promise.value(Unit)
+            .thenMap(on = DispatchExecutor.global) {
+                semaphore.acquire()
+                execute()
+            }
+            .ensure(on = DispatchExecutor.global) {
+                semaphore.release()
+            }
+    }
 }
